90a0c1960e37b33c8e045c76b1326301f482e882
Patrick Wang <1263870+patrickkwang@users.noreply.github.com>
2023-03-27 15:18:00 -0400

    Initial commit

---
+++ src/example.py
@@ -0,0 +1,20 @@
+"""Test example.
+
+Other things.
+"""
+
+
+def main() -> None:
+    """Run the example."""
+    pass
+
+
+def do_something() -> None:
+    """Do something else."""
+    a = 5
+    b = a * 3
+    c = a + b
+
+
+if __name__ == "__main__":
+    main()
